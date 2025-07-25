5714f949b0fb40389876e02ff93a7d03a1d6530a
Frederick <fredmtonline@gmail.com>
2025-07-25 13:31:29 +0530

    Change total size allowed for zip file to be 100 MB

--- src/main/java/com/tofutracker/Coremods/services/ArchiveConstants.java
+++ src/main/java/com/tofutracker/Coremods/services/ArchiveConstants.java
@@ -6,7 +6,7 @@ public class ArchiveConstants {
     public static final Set<String> ARCHIVE_EXTENSIONS = Set.of(
             ".zip", ".rar", ".7z", ".tar", ".gz", ".bz2", ".xz", ".tar.gz", ".tar.bz2", ".tar.xz");
 
-    public static final long MAX_TOTAL_SIZE = 650000000L; // 650 MB
-    public static final int MAX_FILE_COUNT = 100;
+    public static final long MAX_TOTAL_SIZE = 100000000L; // 100 MB
+    public static final int MAX_FILE_COUNT = 250;
     public static final double MAX_COMPRESSION_RATIO = 10.0;
 } 
